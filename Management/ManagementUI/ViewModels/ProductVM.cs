7617b3df7db7292dbabe8cfd737e8b78d8042f9c
Richard Sobreiro <richardsobreiro@gmail.com>
2022-12-25 13:18:45 -0300

    Remove limits on search products

--- Management/ManagementUI/ViewModels/ProductVM.cs
+++ Management/ManagementUI/ViewModels/ProductVM.cs
@@ -7,5 +7,8 @@
         public string Description { get; set; } = "";
         public string? MeasurementUnitId { get; set; }
         public List<ConcentrationVM> SelectedDrugsConcentrations = new();
+        public decimal PurchasePrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public int TotalStockQuantity { get; set; }
     }
 }
